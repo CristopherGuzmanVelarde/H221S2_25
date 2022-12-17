19946e7ca82af37e629c4cc98daf579feffdc286
Cristopher <cristopher.guzman@vallegrande.edu.pe>
2022-12-17 00:59:34 -0500

    Conexión

---
+++ connect.js
@@ -0,0 +1,54 @@
+var express = require("express");
+var mysql = require("mysql");
+var app = express();
+var cors = require("cors");
+
+app.use(express.json());
+app.use(cors());
+
+var conexion = mysql.createConnection({
+  host: "18.207.80.48",
+  user: "cristopher",
+  password: "cris123",
+  database: "dbRestaurante"
+});
+
+conexion.connect(function (error) {
+  if (error) {
+    throw error;
+  } else {
+    console.log("Conexión exitosa");
+  }
+});
+
+const puerto = process.env.PUERTO || 3000;
+
+app.listen(puerto, function () {
+  console.log("Servidor funcionando en puerto: " + puerto);
+});
+
+app.post("/api/pedido", (req, res) => {
+	let data = {
+    	userped: req.body.USERPED,
+    	emausped: req.body.EMAUSPED,
+    	celusped: req.body.CELUSPED,
+    	foodped: req.body.FOODPED,
+    	msgped: req.body.MSGPED
+	};
+	let sql = "INSERT INTO pedido SET ?";
+	conexion.query(sql, data, function (error, results) {
+  	if (error) {
+    	throw error;
+  	} else {
+    	console.log(data);
+    	res.send(data);
+  	}
+	});
+});
+
+const path = require("path");
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'static')));
+app.use(express.static(__dirname + '/'));
